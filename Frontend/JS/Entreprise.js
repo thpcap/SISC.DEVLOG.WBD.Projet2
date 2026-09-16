88fc857bc78e8c3dd8a99f18fbfab735286e8c6c
Thomas CAPRON <thomascapron59@free.fr>
2026-09-16 17:44:59 +0200

    tmpcommit

--- Frontend/JS/Entreprise.js
+++ Frontend/JS/Entreprise.js
@@ -10,15 +10,6 @@ function reloadTable(){
         return response.json();
     })
     .then(data=>{
-        //filter
-        let nameSearch = entrepriseInput.value;
-        if(nameSearch.length!=0){
-            let regex=new RegExp(nameSearch+"*", "g")
-            data=data.filter(ent=>{
-                let res =regex.test(ent.nom);
-                return res;
-            });
-        }
         //emptyTable
         tableBodyEntreprise.innerHTML="";
         //reloadTable
@@ -28,7 +19,7 @@ function reloadTable(){
                 let row = document.createElement("tr");
                 row.innerHTML=` 
                 <td>
-                    <div class="company-identity">
+                    <div class="company-identity" data-name="`entrreprise.nom`">
                         <img src="https://brandemia.org/contenido/subidas/2022/11/tipografia-y-paleta-de-color.png" alt="" class="company-logo">
                         <strong>`+entrreprise.nom+`</strong>
                     </div>
@@ -49,3 +40,17 @@ function reloadTable(){
 document.addEventListener("DOMContentLoaded",function(){
     reloadTable();
 });
+
+
+
+function filter(int){
+     //filter
+    let nameSearch = entrepriseInput.value;
+    if(nameSearch.length!=0){
+        let regex=new RegExp(nameSearch+"*", "gmi")
+        data=data.filter(ent=>{
+            let res =regex.test(ent.nom);
+            return res;
+        });
+    }
+}
