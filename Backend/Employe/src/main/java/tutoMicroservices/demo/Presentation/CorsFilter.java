67abe5b625f297c76ca8b22bcaa91022023c658f
Thomas CAPRON <thomascapron59@free.fr>
2026-09-21 11:30:20 +0200

    added front detals handeling and mock filters for entreprises page

--- Backend/Employe/src/main/java/tutoMicroservices/demo/Presentation/CorsFilter.java
+++ Backend/Employe/src/main/java/tutoMicroservices/demo/Presentation/CorsFilter.java
@@ -1,11 +1,11 @@
 package tutoMicroservices.demo.Presentation;
 
-import java.io.IOException;
-
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerResponseContext;
 import jakarta.ws.rs.container.ContainerResponseFilter;
 import jakarta.ws.rs.ext.Provider;
+
+import java.io.IOException;
 
 @Provider
 public class CorsFilter implements ContainerResponseFilter {
@@ -13,19 +13,19 @@ public class CorsFilter implements ContainerResponseFilter {
     @Override
     public void filter(
             ContainerRequestContext requestContext,
-            ContainerResponseContext responseContext) {
+            ContainerResponseContext responseContext) throws IOException {
 
-        responseContext.getHeaders().add(
+        responseContext.getHeaders().putSingle(
             "Access-Control-Allow-Origin",
             "*"
         );
 
-        responseContext.getHeaders().add(
+        responseContext.getHeaders().putSingle(
             "Access-Control-Allow-Methods",
             "GET, POST, PUT, DELETE, OPTIONS"
         );
 
-        responseContext.getHeaders().add(
+        responseContext.getHeaders().putSingle(
             "Access-Control-Allow-Headers",
             "Content-Type, Authorization"
         );
